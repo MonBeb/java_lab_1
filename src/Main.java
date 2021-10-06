61a2e4d125fef9b9ff2a68a2c5e4ba110c3eb1b8
Monika <monika.bebak@gmail.com>
2021-10-06 12:26:23 +0200

    ćw_3

--- src/Main.java
+++ src/Main.java
@@ -4,87 +4,65 @@ public class Main {
 
     public static void main(String[] args) {
 
-        /* ćw.2 */
+        /* ćw.3 */
 
-        /*   zad.1 zadeklaruj i zainicjalizuj zmienne różnych typów i wyświetl w konsoli (im więcej tym lepiej ;) ) */
+        /*  zad.1 napisać interfejs który bedzie słuzył do wpisania danych osobowych:
+         *   a) Imię, nazwisko, wiek, nr indeksu,
+         *   dane mają być wprowadzane z klawiatury w konsoli
+         *   b) wyświetlić dane za pomocą println i printf
+         */
 
-        /*
-         *    zad.2 Napisz kod który będzie wykonywał operacje: dodawania, odejmowania,
-         *    mnożenia, dziealenia i modulo na zmiennych:
-         *    a) liczbaA i liczbaB, dowolne liczby calkowite (int),
-         *    b) liczbaX i liczbaY, dowolne liczby zmiennoprzecinkowe (double)
-         *  */
-        /* Zadanie 1 */
+        Scanner scan = new Scanner(System.in);
 
-        byte zmiennaA = 12;
-        System.out.println("Oto zmienna byte = " + zmiennaA);
+        System.out.println("Podaj imię:");
+        String imię = scan.next();
 
-        short zmiennaB = 1234;
-        System.out.println("Oto zmienna short = " + zmiennaB);
+        System.out.println("Podaj nazwisko: ");
+        String nazwisko = scan.next();
 
-        int zmiennaC = 12345;
-        System.out.println("Oto zmienna int = " + zmiennaC);
+        System.out.println("Podaj wiek: ");
+        int wiek = scan.nextInt();
 
-        long zmiennaD = 1000000000000L;
-        System.out.println("Oto zmienna long = " + zmiennaD);
+        System.out.println("Podaj numer indeksu: ");
+        int indeksu = scan.nextInt();
 
-        float zmiennaE = 12.34f;
-        System.out.println("Oto zmienna float = " + zmiennaE);
+        System.out.printf("Twoje imię to %s, nazwisko to %s a wiek to %d   \n", imię, nazwisko, wiek);
 
-        double zmiennaF = 123456.1234;
-        System.out.println("Oto zmienna double = " + zmiennaF);
+        System.out.println("Kłamiesz!! Twój wiek to nie " + wiek + " a dokładnie " + (wiek + 10));
 
-        boolean zmiennaG = false;
-        System.out.println("Oto zmienna boolean czyli " + zmiennaG);
 
-        char zmiennaH = 'f';
-        System.out.println("Oto zmienna char czyli " + zmiennaH);
+        /*  zad.2 zadeklarować 2 zmienne typu int i wykonać na nich obliczenia arytmetyczn (+, -, *, /, %)
+         *   wynik wyświetlić w konsoli programu
+         */
 
-        String zmiennaI = "Zadanie 2";
-        System.out.println("Oto zmienna String czyli " + zmiennaI);
-
-        /* Zadanie 2a */
-
-        int a = 100;
-        int b = 55;
+        int a = 111;
+        int b = 222;
 
         int wynikDodawanie = a + b;
-        System.out.println(wynikDodawanie);
-
-        int wynikOdejmowanie = a - b;
-        System.out.println(wynikOdejmowanie);
-
-        int wynikIloczyn;
-        wynikIloczyn = a * b;
-        System.out.println(wynikIloczyn);
-
-        int wynikDzielenie = a/b;
-        System.out.println(wynikDzielenie);
-
+        int wynikOdejmowanie = b - a;
+        int wynikMnożenie = a * b;
+        int wynikDzielenie = b/a;
         int wynikModulo = a % b;
-        System.out.println(wynikModulo);
 
-        /* Zadanie 2b */
+        System.out.println("Podaj wynik dodawania: 111 + 222 = ");
+        int suma = scan.nextInt();
+        System.out.println("Wynik to " + wynikDodawanie);
 
-        double c = 220022.1234;
-        double d = 110011.4321;
+        System.out.println("Podaj wynik odejmowania: 111 - 222 = ");
+        int różnica = scan.nextInt();
+        System.out.println("Wynik to " + wynikOdejmowanie);
 
-        double dodawanie = c + d;
-        System.out.println(dodawanie);
+        System.out.println("Podaj wynik mnożenia: 111 * 222 = ");
+        int iloczyn = scan.nextInt();
+        System.out.println("Wynik to " + wynikMnożenie);
 
-        double odejmowanie = c - d;
-        System.out.println(odejmowanie);
+        System.out.println("Podaj wynik dzielenia: 111/222 = ");
+        int iloraz = scan.nextInt();
+        System.out.println("Wynik to " + wynikDzielenie);
 
-        double iloczyn = c * d;
-        System.out.println(iloczyn);
-
-        double dzielenie = c/d;
-        System.out.println(dzielenie);
-
-        double modulo = c%d;
-        System.out.println(modulo);
+        System.out.println("Podaj wynik modulo: 111 % 222 = ");
+        int modulo2 = scan.nextInt();
+        System.out.println("Wynik to " + wynikModulo);
 
     }
 }
-
-
