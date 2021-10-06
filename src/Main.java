17241dd476afb24bd7b81b10ddc50c17bf148740
Monika <monika.bebak@gmail.com>
2021-10-06 15:30:51 +0200

    ćw_4

--- src/Main.java
+++ src/Main.java
@@ -4,65 +4,29 @@ public class Main {
 
     public static void main(String[] args) {
 
-        /* ćw.3 */
+        /* ćw.4 */
 
-        /*  zad.1 napisać interfejs który bedzie słuzył do wpisania danych osobowych:
-         *   a) Imię, nazwisko, wiek, nr indeksu,
-         *   dane mają być wprowadzane z klawiatury w konsoli
-         *   b) wyświetlić dane za pomocą println i printf
-         */
+        /* zad.1 proszę dodać kilka złożonych operacji uzywając kilku operatorów arytmetycznych i logicznych */
 
-        Scanner scan = new Scanner(System.in);
+        boolean a = true;
+        boolean b = false;
+        boolean c = false;
 
-        System.out.println("Podaj imię:");
-        String imię = scan.next();
+        int x = 10;
+        int y = 20;
+        int z = 30;
 
-        System.out.println("Podaj nazwisko: ");
-        String nazwisko = scan.next();
-
-        System.out.println("Podaj wiek: ");
-        int wiek = scan.nextInt();
-
-        System.out.println("Podaj numer indeksu: ");
-        int indeksu = scan.nextInt();
-
-        System.out.printf("Twoje imię to %s, nazwisko to %s a wiek to %d   \n", imię, nazwisko, wiek);
-
-        System.out.println("Kłamiesz!! Twój wiek to nie " + wiek + " a dokładnie " + (wiek + 10));
+        System.out.println("(b == a) || (x == z): " + ((b == a) || (x == z)));
+        System.out.println("(z > y) || (y > x): " + ((z > y) || (y > x)));
+        System.out.println("(z >= x) || (b != c): " + ((z >= x) || (b != c)));
+        System.out.println("(b || c) && (x < y): " + ((b || c) && (x < y)));
+        System.out.println("(y > x) || (a && b): " + ((y > x) || (a && b)));
 
 
-        /*  zad.2 zadeklarować 2 zmienne typu int i wykonać na nich obliczenia arytmetyczn (+, -, *, /, %)
-         *   wynik wyświetlić w konsoli programu
-         */
 
-        int a = 111;
-        int b = 222;
 
-        int wynikDodawanie = a + b;
-        int wynikOdejmowanie = b - a;
-        int wynikMnożenie = a * b;
-        int wynikDzielenie = b/a;
-        int wynikModulo = a % b;
 
-        System.out.println("Podaj wynik dodawania: 111 + 222 = ");
-        int suma = scan.nextInt();
-        System.out.println("Wynik to " + wynikDodawanie);
 
-        System.out.println("Podaj wynik odejmowania: 111 - 222 = ");
-        int różnica = scan.nextInt();
-        System.out.println("Wynik to " + wynikOdejmowanie);
-
-        System.out.println("Podaj wynik mnożenia: 111 * 222 = ");
-        int iloczyn = scan.nextInt();
-        System.out.println("Wynik to " + wynikMnożenie);
-
-        System.out.println("Podaj wynik dzielenia: 111/222 = ");
-        int iloraz = scan.nextInt();
-        System.out.println("Wynik to " + wynikDzielenie);
-
-        System.out.println("Podaj wynik modulo: 111 % 222 = ");
-        int modulo2 = scan.nextInt();
-        System.out.println("Wynik to " + wynikModulo);
 
     }
 }
