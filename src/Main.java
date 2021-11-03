82b75fe4667696a7029e0015737cbaf0a7f84390
Monika <monika.bebak@gmail.com>
2021-11-03 17:02:34 +0100

    message

--- src/Main.java
+++ src/Main.java
@@ -4,29 +4,43 @@ public class Main {
 
     public static void main(String[] args) {
 
-        /* ćw.4 */
+        /* ćw.5 */
 
-        /* zad.1 proszę dodać kilka złożonych operacji uzywając kilku operatorów arytmetycznych i logicznych */
+        /* zad.1 podziel swój wiek przez modulo 3 i jeśli twój wiek zwraca 0 wyświetl napis "Podzielny przez 3",
+         * w innym wypadku "Niepodzielny przez 3" użyj kontrukcji if else
+         * */
+        int zmiennaWiek = 27;
+        int zmiennaWynik = (zmiennaWiek % 3 == 0) ? 0 : 1;
 
-        boolean a = true;
-        boolean b = false;
-        boolean c = false;
+        if (zmiennaWynik == 0) {
+            System.out.println("Podzielny przez 3");
+        } else {
+            System.out.println("Niepodzielny przez 3");
+        }
+        /* zad.2 użyj parametru trójargumentowego aby sprawdzić parzystośc nr. indeksu */
 
-        int x = 10;
-        int y = 20;
-        int z = 30;
+        int zmiennaIndeks = 121212;
+        int zmiennaParzysta = (zmiennaIndeks % 2 == 0) ? 0 : 1;
+        if (zmiennaParzysta == 0) {
+            System.out.println("Numer indeksu jest parzysty " + zmiennaParzysta + ".");
+        } else {
+            System.out.println("Numer indeksu jest nieparzysty " + zmiennaParzysta + ".");
+        }
 
-        System.out.println("(b == a) || (x == z): " + ((b == a) || (x == z)));
-        System.out.println("(z > y) || (y > x): " + ((z > y) || (y > x)));
-        System.out.println("(z >= x) || (b != c): " + ((z >= x) || (b != c)));
-        System.out.println("(b || c) && (x < y): " + ((b || c) && (x < y)));
-        System.out.println("(y > x) || (a && b): " + ((y > x) || (a && b)));
+        /* zad.3 wpisz z klawiatury liczbę zmiennoprzecinkową (użyj klasy Scanner z poprzednich ćwiczeń)
+         * i stwórz konstrukcję else if z dowolnymi komunikatami */
+        Scanner scan = new Scanner(System.in);
 
+        System.out.print("Napisz liczbę, o której myślę (max. 5 cyfr): ");
+        float liczba = scan.nextFloat();
 
+        if (liczba == 55555) {
+            System.out.println("Doskonale!!! Właśnie o tej liczbie myślałam!!!");
 
-
-
-
-
+        } else if (liczba < 55555) {
+        System.out.println("Niestety... Liczba, o której myślałam jest większa.");
+        } else if (liczba > 55555) {
+        System.out.println("Niestety... Liczba, o której myślałam jest mniejsza.");
+        }
     }
 }
